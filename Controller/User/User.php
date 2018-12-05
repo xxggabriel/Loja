065015968e916f7519d27417f5444016990bb20d
Gabriel Oliveira <gabrieloliveradesouza9@gmail.com>
2018-12-05 15:37:30 -0200

    GETs and SETs criados com validação de dados

--- Controller/User/User.php
+++ Controller/User/User.php
@@ -3,24 +3,27 @@
 namespace Controller\User;
 
 use Model\Crud;
-use Model\Model;
+use Controller\Exceptions\ExceptionsUser;
+class User extends Crud {
 
-class User extends Crud{
-
-    private $model;
-    private $user;
+    private $name;
+    private $login;
+    private $email;
+    private $phone;
 
     public function createUser($data = array()){
-         
-        $this->setData($data);
 
-        // var_dump($this->getlogin());exit;
+        
+        $this->setName($data["name"]) ;
+        $this->setLogin($data["login"]) ;
+        $this->setEmail($data["email"]);
+        $this->setPhone($data["phone"]);
 
         $this->create("tb_user", [
-            "name" => $this->getname(),
-            "login" => $this->getlogin(),
-            "email" => $this->getemail(),
-            "phone" => $this->getphone(),
+            "name" => $this->getName(),
+            "login" => $this->getLogin(),
+            "email" => $this->getEmail(),
+            "phone" => $this->getPhone(),
             "status" => 1
 
         ]);
@@ -48,6 +51,86 @@ class User extends Crud{
     public function deleteUser($id_user){
 
         $this->delete("tb_user", "id_user = $id_user");
+
+    }
+
+
+    // GETs
+    public function getName(){
+
+        return $this->name;
+
+    }
+
+    public function getLogin(){
+
+        return $this->login;
+
+    }
+
+    public function getEmail(){
+
+        return $this->email;
+
+    }
+
+    public function getPhone(){
+
+        return $this->phone;
+
+    }
+
+
+    // SETs
+    public function setName($name){
+        if(!empty($name)){
+            // deixando todas tudo maiúscula 
+            $name = strtolower($name);
+            // Convertendo toda a primeira letra em maiúscula
+            $name = ucwords($name);
+
+            $this->name = $name;
+        } else {
+            ExceptionsUser::nameNotInformed;
+        }
+
+    }
+
+    public function setLogin($login){
+
+        if(!empty($login)){
+
+            $this->login = $login;
+        } else{
+            ExceptionsUser::loginNotInformed;
+        }
+
+    }
+
+    public function setEmail($email){
+
+        if(!empty($email)){
+            // Validar email
+            if(filter_var($email, FILTER_VALIDATE_EMAIL)){
+                
+                $this->email = $email;
+    
+            } else{
+    
+                ExceptionsUser::invalidEmail();    
+    
+            }
+        } else {
+            ExceptionsUser::emailNotInformed;
+        }
+
+    }
+
+    public function setPhone($phone){
+
+        // Retirar todos os caracters do numero
+        $phone = str_replace(["(",")","-"," "], "", $phone);
+        $this->phone = $phone;
 
     }
 
