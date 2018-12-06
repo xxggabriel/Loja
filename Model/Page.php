033327c72d799676d3535017fd1398bb8f7c1759
Gabriel Oliveira <gabrieloliveradesouza9@gmail.com>
2018-12-06 03:41:31 -0200

    Classe Page criada, para renderização dos tamplates da Views

---
+++ Model/Page.php
@@ -0,0 +1,63 @@
+<?php 
+
+namespace Model;
+
+use \Rain\Tpl;
+
+class Page{
+
+    private $tpl;
+    private $values = [];
+    private $default = [
+        "header" => true,
+        "footer" => true
+    ];
+    private $options = [];
+
+    public function __construct($opts = array()){
+
+        $this->options = array_marge($default,$opts);
+
+        // config
+        $config = array(
+            "tpl_dir"       => $_SERVER["DOCUMENT_ROOT"]."Views",
+            "cache_dir"     => $_SERVER["DOCUMENT_ROOT"]."Views/cache/",
+            "debug"         => true, // set to false to improve the speed
+        );
+
+        Tpl::configure( $config );
+
+        $this->tpl = new Tpl();
+
+        if($this->options["header"] === true) {
+            $this->tpl->draw("header");
+        }
+    }
+
+    public function setTpl($tpl, $data = array()){
+
+        $this->setVariables($data);
+
+        $this->tpl->draw($tpl);
+
+    }
+
+    private function setVariables($data = array()){
+
+        foreach($data as $key => $value){
+
+            $this->tpl->assign($key,$value);
+
+        }
+
+    }
+
+    public function __destruct(){
+        if($this->options["footer"] === true) {
+            $this->tpl->draw("footer");
+        }   
+    }
+
+
+
+}
