55918b3181d0c8d0306efe8643a4c316e7370dc1
Gabriel Oliveira <gabrieloliveradesouza9@gmail.com>
2018-12-06 03:20:45 -0200

    RainTpl adicionado

--- vendor/composer/autoload_namespaces.php
+++ vendor/composer/autoload_namespaces.php
@@ -6,4 +6,5 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
+    'Rain' => array($vendorDir . '/rain/raintpl/library'),
 );
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -28,11 +28,22 @@ class ComposerStaticInit939ae647a818e1e42926b45dceab4b66
         ),
     );
 
+    public static $prefixesPsr0 = array (
+        'R' => 
+        array (
+            'Rain' => 
+            array (
+                0 => __DIR__ . '/..' . '/rain/raintpl/library',
+            ),
+        ),
+    );
+
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
             $loader->prefixLengthsPsr4 = ComposerStaticInit939ae647a818e1e42926b45dceab4b66::$prefixLengthsPsr4;
             $loader->prefixDirsPsr4 = ComposerStaticInit939ae647a818e1e42926b45dceab4b66::$prefixDirsPsr4;
+            $loader->prefixesPsr0 = ComposerStaticInit939ae647a818e1e42926b45dceab4b66::$prefixesPsr0;
 
         }, null, ClassLoader::class);
     }
