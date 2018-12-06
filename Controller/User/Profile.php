cdc53ecdaf0232674f13bc4cd13d6cd051f2d090
Gabriel Oliveira <gabrieloliveradesouza9@gmail.com>
2018-12-06 01:02:50 -0200

    CRUD da classe Profile criada com validação de parâmetro

---
+++ Controller/User/Profile.php
@@ -0,0 +1,161 @@
+<?php 
+
+namespace Controller\User;
+
+use Model\Crud;
+use Controller\Exceptions\ExceptionsUserProfile;
+
+class Profile extends Crud{
+   
+    private $id_user;
+    private $user_name;
+    private $photo;
+    private $biography;
+
+    public function createProfile($data = array()){
+
+        $this->setId_user($data["id_user"]);
+        $this->setUser_name($data["user_name"]);
+        $this->setPhoto($data["photo"]);
+        $this->setBiography($data["biography"]);
+
+        $this->create("tb_user_profile", [
+            "id_user" => $this->getId_user(),
+            "user_name" => $this->getUser_name(),
+            "photo" => $this->photo(),
+            "biography" => $this->getBiography()
+        ]);
+    }
+
+    public function selectProfile($colunms, $where, $table = "tb_user_profile"){
+        
+        $result = $this->read($table, $colunms, $where);
+
+        return $result;
+
+    }
+
+    public function selectProfileAll($colunms, $table = "tb_user_profile"){
+
+        $result = $this->read($table, $colunms, "1 = 1");
+
+        return $result;
+
+    }
+
+    public function updateProfile($data, $id_user){
+
+        $this->update("tb_user_profile",$data, "id_user = '$id_user'");
+
+    }
+
+    public function deleteProfile($id_user){
+
+        $this->delete("tb_user_profile", "id_user = '$id_user'");
+
+    }
+
+    
+
+
+
+    public function getId_user()
+    {
+        return $this->id_user;
+    }
+
+
+    public function setId_user($id_user)
+    {
+        if(!empty($id_user)){
+            $result = $this->selectProfile("id_user","id_user = '$id_user'","tb_user");
+
+            if(in_array($result[0],$result)){
+
+                $result = $this->selectProfile("id_user","id_user = '$id_user'");
+                
+                if(!in_array($result[0],$result)){
+                   
+                    $this->id_user = $id_user;
+
+                } else {
+
+                    ExceptionsUserProfile::userIdExisting();
+
+                }
+            
+            } else {
+
+                ExceptionsUserProfile::idNonExistent();
+
+            }
+
+        } else{
+
+            ExceptionsUserProfile::notInformedIdUser();
+
+        }
+    }
+
+
+    public function getUser_name()
+    {
+        return $this->user_name;
+    }
+
+
+    public function setUser_name($user_name)
+    {
+        if(!empty($user_name)){
+
+            // Varifica na tabela TB_USER_PROFILE se exite algum usuario com user name igual ao passado
+            $result = $this->selectProfile("user_name", "user_name = '$user_name'");
+            if(!in_array($result[0], $result)){
+    
+                $this->user_name = $user_name;
+                
+    
+            } else {
+                ExceptionsUserProfile::existingUser();
+            }
+        } else {
+            ExceptionsUserProfile::notInformedUserName();
+        }
+    }
+
+
+    public function getPhoto()
+    {
+        return $this->photo;
+    }
+
+
+    public function setPhoto($photo)
+    {
+        if(!empty($photo)){
+
+            $this->photo = $photo;
+            
+        } else {
+            $this->photo = NULL;
+        }
+    }
+
+  
+    public function getBiography()
+    {
+        return $this->biography;
+    }
+
+
+    public function setBiography($biography)
+    {
+        if(!empty($biography)){
+
+            $this->biography = $biography;
+
+        } else {
+            $this->biography = NULL;
+        }
+    }
+}
