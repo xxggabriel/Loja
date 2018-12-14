c8736203516d645374640938c51da39b58540a73
Gabriel Oliveira <gabrieloliveradesouza9@gmail.com>
2018-12-13 22:34:15 -0200

    Classe admin com todas os recursos para Route/Admin

--- src/Controller/User/Admin.php
+++ src/Controller/User/Admin.php
@@ -40,49 +40,21 @@ class Admin extends User{
         }
     }
 
-    
-    public static function listAllUser(){
-        $user = new User();
-        return $user->selectUserAll("status > 0");
-    }
-
-    public static function listUser($id_user){
-        $user = new User();
-        $result = $user->selectUser($id_user);
-        return $result[0];
-    }
-
-
-    public static function listAllProduct(){
-        $product = new Product();
-       
-        return  $product->selectAllProducts();
-        
-    }
-
-    public static function listProductSample($id_product){
-        $product = new Product();
-       
-        return  $product->selectProductSample($id_product);
-    }
-
-    public static function listAllBrand(){
-        $brand = new Brand();
-        return $brand->selectAllBrand();
-    }
-
-    public static function listProduct($id_product){
-        $product = new Product();
-        $result = $product->selectProduct($id_product);
-        return $result[0];
-    }
-
     public static function createUserAdmin($data = array(), $route = "/admin/users"){
 
         $user = new User();
         $user->createUser($data);
         header("Location: $route");
         exit;
+    }
+    
+    public static function createProductSampleAdmin($data = array(), $route = "/admin/products"){
+
+        $product = new Product();
+        $product->createProductSample($data);
+        header("Location: $route");
+        exit;
+
     }
 
     public static function createProductAdmn($data = array(), $route = "/admin/products"){
@@ -92,9 +64,46 @@ class Admin extends User{
         exit;
     }
 
+    public static function createProviderAdmin($data = array(), $route = "/admin/providers"){
+
+        $provider = new Provider();
+        $provider->createProvider($data);
+        header($route);
+        exit;
+
+    }
+
+    public static function createBrandAdmin($data = array(), $route = "/admin/brands"){
+        $brand = new Brand();
+        $brand->createBrand($data);
+        header("Location: $route");
+        exit;
+    }
+
+    public static function createTypeAdmin($data = array(), $route = "/admin/types"){
+        $type = new Type();
+        $type->createType($data);
+        header("Location: $route");
+        exit;
+    }
+
     public static function updateUserAdmin($data = array(),$id_user, $route = "/admin/users"){
         $user = new User();
         $user->updateUser($data,$id_user);
+        header("Location: $route");
+        exit;
+    }
+
+    public static function updateBrandAdmin($data = array(),$id_brand, $route = "/admin/brands"){
+        $brand = new Brand();
+        $brand->updateBrand($data,$id_brand);
+        header("Location: $route");
+        exit;
+    }
+
+    public static function updateTypeAdmin($data = array(),$id_type, $route = "/admin/types"){
+        $type = new Type();
+        $type->updatetype($data,$id_type);
         header("Location: $route");
         exit;
     }
@@ -113,6 +122,15 @@ class Admin extends User{
         exit;
     }
 
+    public static function updateProviderAdmin($data = array(), $id_provider, $route = "/admin/providers"){
+
+        $provider = new Provider();
+        $provider->updateProvider($data, $id_provider);
+        header("Location: $route");
+        exit;
+
+    }
+
     public static function deleteAdminUser($id_user, $route = "/admin/users"){
         $user = new User();
         $user->deleteUser($id_user);
@@ -127,14 +145,12 @@ class Admin extends User{
         exit;
     }
 
-    public static function createProductSampleAdmin($data = array(), $route = "/admin/products"){
-
-        $product = new Product();
-        $product->createProductSample($data);
+    public static function deleteProviderAdmin($id_provider, $route = "/admin/providers"){
+        $provider = new Provider();
+        $provider->deleteProvider($id_provider);
         header("Location: $route");
         exit;
-
-    }
+    } 
 
     public static function savePhoto($data){
         $uploaddir = $_SERVER["DOCUMENT_ROOT"].'/resource/upload/';
@@ -179,29 +195,41 @@ class Admin extends User{
         return $result[0];
     }
 
-    public static function createProviderAdmin($data = array(), $route = "/admin/providers"){
-
-        $provider = new Provider();
-        $provider->createProvider($data);
-        header($route);
-        exit;
-
+    
+    public static function listAllUser(){
+        $user = new User();
+        return $user->selectUserAll("status > 0");
     }
 
-    public static function deleteProviderAdmin($id_provider, $route = "/admin/providers"){
-        $provider = new Provider();
-        $provider->deleteProvider($id_provider);
-        header("Location: $route");
-        exit;
-    } 
+    public static function listUser($id_user){
+        $user = new User();
+        $result = $user->selectUser($id_user);
+        return $result[0];
+    }
 
-    public static function updateProviderAdmin($data = array(), $id_provider, $route = "/admin/providers"){
 
-        $provider = new Provider();
-        $provider->updateProvider($data, $id_provider);
-        header("Location: $route");
-        exit;
+    public static function listAllProduct(){
+        $product = new Product();
+       
+        return  $product->selectAllProducts();
+        
+    }
 
+    public static function listProductSample($id_product){
+        $product = new Product();
+       
+        return  $product->selectProductSample($id_product);
+    }
+
+    public static function listAllBrand(){
+        $brand = new Brand();
+        return $brand->selectAllBrand();
+    }
+
+    public static function listProduct($id_product){
+        $product = new Product();
+        $result = $product->selectProduct($id_product);
+        return $result[0];
     }
 
 
