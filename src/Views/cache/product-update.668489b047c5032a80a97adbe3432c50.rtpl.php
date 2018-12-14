08cd20d21ceb2027eabb805e58cf9dbeaed53012
Gabriel Oliveira <gabrieloliveradesouza9@gmail.com>
2018-12-13 22:32:59 -0200

    Listando os tipos, fornecedores, marcas, no editar

--- src/Views/cache/product-update.668489b047c5032a80a97adbe3432c50.rtpl.php
+++ src/Views/cache/product-update.668489b047c5032a80a97adbe3432c50.rtpl.php
@@ -21,51 +21,52 @@
                 <form role="form" action="/admin/product/<?php echo htmlspecialchars( $product["id_product"], ENT_COMPAT, 'UTF-8', FALSE ); ?>" method="post">
                   <div class="box-body">
                     
-                    <!-- <div class="form-group">
-                      <label for="provider">Fornecedor</label>
-                      <input type="number" class="form-control" id="provider" name="id_provider" placeholder="Digite o nome" value="<?php echo htmlspecialchars( $product["id_provider"], ENT_COMPAT, 'UTF-8', FALSE ); ?>">
-                    </div> -->
+                    
 
                     <div class="form-group">
                       <label for="provider">Fornecedor</label>
-                      <select class="form-control" name="provider" id="provider">
-                      
+                      <select class="form-control" name="id_provider" id="provider">
+
+                          <?php if( empty($product["id_provider"]) ){ ?>
+                          <option value="NULL">Sem marca</option>
+                          <?php }else{ ?>
+                          <option value="<?php echo htmlspecialchars( $product["id_provider"], ENT_COMPAT, 'UTF-8', FALSE ); ?>"><?php $k = $product["id_provider"] - 1; ?><?php echo htmlspecialchars( $provider["$k"]["name_provider"], ENT_COMPAT, 'UTF-8', FALSE ); ?> </option>
+                          <?php } ?>
                         <?php $counter1=-1;  if( isset($provider) && ( is_array($provider) || $provider instanceof Traversable ) && sizeof($provider) ) foreach( $provider as $key1 => $value1 ){ $counter1++; ?>
-                        <option><?php echo htmlspecialchars( $value1["$key1"]["name_provider"], ENT_COMPAT, 'UTF-8', FALSE ); ?></option>
-                        
+                        <option value="<?php echo htmlspecialchars( $value1["id_provider"], ENT_COMPAT, 'UTF-8', FALSE ); ?>"><?php echo htmlspecialchars( $value1["name_provider"], ENT_COMPAT, 'UTF-8', FALSE ); ?></option>                    
                         <?php } ?>
                      
                       </select>
                     </div>
 
-                    <!-- <div class="form-group">
-                      <label for="brand">Marca</label>
-                      <input type="number" class="form-control" id="brand" name="id_brand" placeholder="Digite o nome" value="<?php echo htmlspecialchars( $product["id_brand"], ENT_COMPAT, 'UTF-8', FALSE ); ?>">
-                    </div> -->
                     <div class="form-group">
-                      <label for="exampleFormControlSelect1">Example select</label>
-                      <select class="form-control" id="exampleFormControlSelect1">
-                        <option>1</option>
-                        <option>2</option>
-                        <option>3</option>
-                        <option>4</option>
-                        <option>5</option>
+                      <label for="brand">Marca</label>
+                      <select class="form-control" name="id_brand" id="brand">
+                        <?php if( empty($product["id_brand"]) ){ ?>
+                        <option value="NULL">Sem marca</option>
+                        <?php }else{ ?>
+                        <option value="<?php echo htmlspecialchars( $product["id_brand"], ENT_COMPAT, 'UTF-8', FALSE ); ?>"><?php $k = $product["id_brand"] - 1; ?><?php echo htmlspecialchars( $brand["$k"]["name_brand"], ENT_COMPAT, 'UTF-8', FALSE ); ?> </option>
+                        <?php } ?>
+                        <?php $counter1=-1;  if( isset($brand) && ( is_array($brand) || $brand instanceof Traversable ) && sizeof($brand) ) foreach( $brand as $key1 => $value1 ){ $counter1++; ?>
+                        <option value="<?php echo htmlspecialchars( $value1["id_brand"], ENT_COMPAT, 'UTF-8', FALSE ); ?>"><?php echo htmlspecialchars( $value1["name_brand"], ENT_COMPAT, 'UTF-8', FALSE ); ?></option>                    
+                        <?php } ?>
+                     
                       </select>
                     </div>
                     
-                    <!-- <div class="form-group">
-                      <label for="desperson">Tipo</label>
-                      <input type="number" class="form-control" id="type" name="id_type" placeholder="Digite o nome" value="<?php echo htmlspecialchars( $product["id_type"], ENT_COMPAT, 'UTF-8', FALSE ); ?>">
-                    </div> -->
-
                     <div class="form-group">
-                      <label for="exampleFormControlSelect1">Example select</label>
-                      <select class="form-control" id="exampleFormControlSelect1">
-                        <option>1</option>
-                        <option>2</option>
-                        <option>3</option>
-                        <option>4</option>
-                        <option>5</option>
+                      <label for="type">Tipo</label>
+                      <select class="form-control" name="id_type" id="type">
+
+                          <?php if( !empty($product["id_type"]) ){ ?>
+                          <option value="<?php echo htmlspecialchars( $product["id_type"] , ENT_COMPAT, 'UTF-8', FALSE ); ?>"><?php $k = $product["id_type"] - 1; ?><?php echo htmlspecialchars( $type["$k"]["name_type"], ENT_COMPAT, 'UTF-8', FALSE ); ?> </option>
+                          <?php }else{ ?>
+                          <option value="NULL">Sem tipo</option>
+                          <?php } ?>
+                        <?php $counter1=-1;  if( isset($type) && ( is_array($type) || $type instanceof Traversable ) && sizeof($type) ) foreach( $type as $key1 => $value1 ){ $counter1++; ?>
+                        <option value="<?php echo htmlspecialchars( $value1["id_type"], ENT_COMPAT, 'UTF-8', FALSE ); ?>"><?php echo htmlspecialchars( $value1["name_type"], ENT_COMPAT, 'UTF-8', FALSE ); ?></option>                    
+                        <?php } ?>
+                     
                       </select>
                     </div>
 
